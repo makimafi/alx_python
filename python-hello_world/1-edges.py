54c59fc2abddb66ddc76a914c7c9d69bf52a9a23
maleda mesfin <markanmesfin555@gmail.com>
2023-11-29 23:25:00 +0300

    creat new file

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+word_first_3 {2,""}
+word_last_2 {1,-1}
+middle_word {3,-2}
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
