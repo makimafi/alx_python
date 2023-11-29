68cf63b21424ffb533684ff3498148bec7b871db
maleda mesfin <markanmesfin555@gmail.com>
2023-11-30 01:26:30 +0300

    creat new file

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    Last_digit = -1 * int(str(number)[-1])
+else:
+    Last_digit = int(str(number)[-1])
+if Last_digit > 5:
+        print("last digit of {} is {} and is greater than 5".format(number,last_digit))
+elif last_digit ==0:
+        print("last digit of{} is{} and is 0".format(number, last_digit))
+elif last_digit < 6 and last_digit != 0:
+        print("last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
