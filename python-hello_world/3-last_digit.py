15a0143d5cf6b58b9e74ae898d4b96bae1c7c535
maleda mesfin <markanmesfin555@gmail.com>
2023-11-30 20:31:37 +0300

    bug fix

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    Last_digit = -1 * int(str(number)[-1])
+    last_digit = -1 * int(str(number)[-1])
 else:
-    Last_digit = int(str(number)[-1])
-if Last_digit > 5:
-        print("last digit of {} is {} and is greater than 5".format(number, last_digit))
+    last_digit = int(str(number)[-1])
+if last_digit > 5:
+    print("last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
-        print("last digit of {} is {} and is 0".format(number, last_digit))
+    print("last digit of {} is {} and is 0".format(number, last_digit))
 elif last_digit < 6 and last_digit != 0:
-        print("last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    print("last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
