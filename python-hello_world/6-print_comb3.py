d9a963b3ca78288e0cfb203550a933d4b7ea6592
maleda mesfin <markanmesfin555@gmail.com>
2023-11-30 14:12:34 +0300

    bug fix

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in rangec(0, 9):
     for j in range (i + 1, 10):
         if i == 8 and j == 9:
             print("{}{}".format(i, j))
-            else:
-                print("{}{}".format (i, j), end=", ")
+        else:
+            print("{}{}".format (i, j), end=", ")
