36df465ae625a40c9453f49c44b9cc5164fb4094
maleda mesfin <markanmesfin555@gmail.com>
2023-11-30 14:15:34 +0300

    bug fix

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range (0, 100):
-for i == 99:
-    print(i)
-    else:
-        print("{:02}".format(i), end=",")
+   if i == 99:
+       print(i)
+   else:
+       print("{:02}".format(i), end=",")
     
