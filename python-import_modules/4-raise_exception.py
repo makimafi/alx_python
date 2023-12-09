5f70ce06a80da4b2bc4b205009d6e7dc31ca17a5
makimafi <149118815+makimafi@users.noreply.github.com>
2023-12-10 00:19:12 +0300

    Update 4-raise_exception.py

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -3,6 +3,6 @@ def rise_exception():
     raise typeError("this is a deliberate typeerror")
 # uncomment the foolowing line if you want to test the function directly
 #try:
-     raise_exception()
+# raise_exception()
 # except TypeError as te:
 #   print("Exception raised")   
