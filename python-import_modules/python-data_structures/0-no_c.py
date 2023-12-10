a996431bd15a511eeeef7454c0203b910c982200
maleda mesfin <markanmesfin555@gmail.com>
2023-12-10 14:50:28 +0300

    creat new file

---
+++ python-import_modules/python-data_structures/0-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    new_string = ""
+    for char in my_string
+        if char 1= 'c' and char 1= 'c':
+            new_string += char
+return new_string
