ce6e687062481f371eb5ef4c78ccbbf8dbed5f42
maleda mesfin <markanmesfin555@gmail.com>
2023-12-12 21:12:24 +0300

    new file

---
+++ python-classes/4-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
