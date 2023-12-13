4050efe56c4f3cccd01390fcc3747fce5a1f6ff9
maleda mesfin <markanmesfin555@gmail.com>
2023-12-13 18:46:55 +0300

    dug fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,4 +3,10 @@ class Square:
     def __init__(self, size):
         self.__size = size  # Private instance attribute
 
+# Example usage:
+# Create an instance of Square with a size of 5
+square_instance = Square(5)
+
+# Access the size attribute (though it's private, the name is _Square__size)
+print(square_instance._Square__size)
 
