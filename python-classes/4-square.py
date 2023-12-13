aeafa69fce432181648d430ee1345a899b68753a
maleda mesfin <markanmesfin555@gmail.com>
2023-12-13 19:55:13 +0300

    dug fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
-my_square = Square(3)
-my_square.my_print()
+class Square:
+    def __init__(self, size=0):
+        self.size = size
 
-print("--")
+    @property
+    def size(self):
+        return self.__size
 
-my_square.size = 10
-my_square.my_print()
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-print("--")
+    def area(self):
+        return self.__size ** 2
 
-my_square.size = 0
-my_square.my_print()
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
 
-print("--")
+# Example usage:
+# square1 = Square(5)
+# print(square1.area())
+# square1.my_print()
