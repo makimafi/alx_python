181547808b32282eb28d873c09a32e08c1e13c76
maleda mesfin <markanmesfin555@gmail.com>
2023-12-13 18:21:41 +0300

    new file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python3
-class square:   
-def _init_(self, size="0"):
-    self.size = size
-    
+#!/usr/bin/pytho
+class Square:
+    def __init__(self, size):
+        self.__size = size  # Private instance attribute
+
+# Example usage:
+# Create an instance of Square with a size of 5
+square_instance = Square(5)
+
+# Access the size attribute (though it's private, the name is _Square__size)
+print(square_instance._Square__size)
+
