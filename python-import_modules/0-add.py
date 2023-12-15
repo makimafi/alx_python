8e9a33cca5bd63abbc5fef3d216037e6eafeb38a
maleda mesfin <markanmesfin555@gmail.com>
2023-12-15 21:36:09 +0300

    creat new file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,23 @@ if __name__=="__main__":
     a = 1
     b = 2
     result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, result))#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
