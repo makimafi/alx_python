a4747ae697af586341a3c47f1b62a6f4047f8f15
maleda mesfin <markanmesfin555@gmail.com>
2023-12-19 20:29:42 +0300

    creat new file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from models.base import Base
 
+class Base:
+    def __init__(self, id):
+        self.id = id
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
