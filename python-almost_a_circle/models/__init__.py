4d21f9ffc834f36a5dfc221638daa101f3476cd0
maleda mesfin <markanmesfin555@gmail.com>
2023-12-19 21:34:51 +0300

    creat new file

--- python-almost_a_circle/models/__init__.py
+++ python-almost_a_circle/models/__init__.py
@@ -1,15 +1,25 @@
 # models/base.py
 
 class Base:
+    """
+    This is the base class for managing id attributes in the project.
+    It automatically assigns unique ids to instances.
+    """
+
     # Private class attribute
     __nb_objects = 0
 
     # Class constructor
     def __init__(self, id=None):
-        # If id is provided, assign it to the public instance attribute id
+        """
+        Initialize the Base class.
+
+        Parameters:
+        - id (int): If provided, assign it to the public instance attribute id.
+                   If not provided, generate a new id using __nb_objects.
+        """
         if id is not None:
             self.id = id
-        # If id is not provided, generate a new id using __nb_objects
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
