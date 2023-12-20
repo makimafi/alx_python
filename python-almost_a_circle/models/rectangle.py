bf8215f8a67073ad707a9944dd03d28e808421b4
maleda mesfin <markanmesfin555@gmail.com>
2023-12-20 20:51:42 +0300

    creat new file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """ Class Rectangle that inherits from Base """
 
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Constructor method """
         super().__init__(id)
@@ -13,6 +14,7 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
 
     @property
     def width(self):
@@ -52,7 +54,32 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ Setter method for y """
-        self.__y = value
+     if __name__ == "__main__":
+
+    try:
+        Rectangle(10, "2")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+   self.__y = value
+
+
 def area(self):
         """ Method to calculate the area of the rectangle """
         return self.__width * self.__height
@@ -110,6 +137,102 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height
         )
+def display(self):
+    """ Method to display the rectangle with '#' characters, considering x and y """
+    for _ in range(self.__y):
+        print()
+    for _ in range(self.__height):
+        print(" " * self.__x + "#" * self.__width)#!/usr/bin/python3
+""" Module for Rectangle class """
+
+from models.base import Base
+
+class Rectangle(Base):
+    """ Class Rectangle that inherits from Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor method """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """ Getter method for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter method for width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter method for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter method for height """
+        self.__height = value
+
+    @property#!/usr/bin/python3
+""" Module for Rectangle class """
+
+from models.base import Base
+
+class Rectangle(Base):
+    """ Class Rectangle that inherits from Base """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor method """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """ Getter method for width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter method for width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter method for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter method for height """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter method for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter method for x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter method for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter method for y """
+        self.__y = value
 
 if __name__ == "__main__":
     r1 = Rectangle(10, 10, 10, 10)
