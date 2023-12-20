7e9fd9986d2e698076c1f9b9a8b33fbaf7892cdb
makimafi <149118815+makimafi@users.noreply.github.com>
2023-12-20 21:43:31 +0300

    Update rectangle.py docmentation requirement error fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,63 +1,128 @@
 #!/usr/bin/python3
-""" Module for Rectangle class """
+"""
+Module: rectangle
+Description: This module contains the definition of the Rectangle class.
+"""
 
 from models.base import Base
 
 class Rectangle(Base):
-    """ Class Rectangle that inherits from Base """
+    """
+    Rectangle class, inherits from Base.
 
+    Attributes:
+    - width (int): Width of the rectangle.
+    - height (int): Height of the rectangle.
+    - x (int): x-coordinate of the rectangle.
+    - y (int): y-coordinate of the rectangle.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Constructor method """
+        """
+        Initializes a new instance of the Rectangle class.
+
+        Args:
+        - width (int): Width of the rectangle.
+        - height (int): Height of the rectangle.
+        - x (int, optional): x-coordinate of the rectangle (default is 0).
+        - y (int, optional): y-coordinate of the rectangle (default is 0).
+        - id (int, optional): The ID to assign to the object (default is None).
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
 
-
     @property
     def width(self):
-        """ Getter method for width """
+        """Getter for the width attribute."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Setter method for width """
+        """Setter for the width attribute."""
         self.__width = value
 
     @property
     def height(self):
-        """ Getter method for height """
+        """Getter for the height attribute."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Setter method for height """
+        """Setter for the height attribute."""
         self.__height = value
 
     @property
     def x(self):
-        """ Getter method for x """
+        """Getter for the x attribute."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ Setter method for x """
+        """Setter for the x attribute."""
         self.__x = value
 
     @property
     def y(self):
-        """ Getter method for y """
+        """Getter for the y attribute."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ Setter method for y """
-     if __name__ == "__main__":
+        """Setter for the y attribute."""
+        self.__y = value
 
+    def area(self):
+        """
+        Calculates and returns the area of the Rectangle instance.
+
+        Returns:
+        - int: The area value.
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        Prints the Rectangle instance with the character '#' to stdout,
+        taking into account the x and y coordinates.
+        """
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns arguments to attributes based on their position in *args
+        or using key/value pairs in **kwargs.
+
+        Args:
+        - *args: Variable number of positional arguments.
+        - **kwargs: Variable number of keyword arguments (key/value pairs).
+        """
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle instance.
+
+        Returns:
+        - str: The string representation.
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+if __name__ == "__main__":
     try:
-        Rectangle(10, "2")
+        r = Rectangle(10, "2")
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
 
@@ -74,211 +139,9 @@ class Rectangle(Base):
         print("[{}] {}".format(e.__class__.__name__, e))
 
     try:
-        Rectangle(10, 2, 3, -1)
+        r = Rectangle(10, 2, 3, -1)
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
-   self.__y = value
-
-
-def area(self):
-        """ Method to calculate the area of the rectangle """
-        return self.__width * self.__height
-def display(self):
-        """ Method to display the rectangle with '#' characters """
-        for _ in range(self.__height):
-            print("#" * self.__width)
-  def __str__(self):
-        """ Method to override the default __str__ method """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height
-        )
-def update(self, *args):
-    """ Method to update attributes based on the order of *args """
-    if args:
-        attributes = ["id", "width", "height", "x", "y"]
-        for i in range(len(args)):
-            setattr(self, attributes[i], args[i])
-def update(self, *args, **kwargs):
-    """ Method to update attributes based on *args and **kwargs """
-    if args:
-        attributes = ["id", "width", "height", "x", "y"]
-        for i in range(len(args)):
-            setattr(self, attributes[i], args[i])
-    elif kwargs:
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-class Rectangle(Base):
-    """ Class Rectangle that inherits from Base """
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ Constructor method """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    # Other methods and properties...
-
-    def update(self, *args, **kwargs):
-        """ Method to update attributes based on *args and **kwargs """
-        if args:
-            attributes = ["id", "width", "height", "x", "y"]
-            for i in range(len(args)):
-                setattr(self, attributes[i], args[i])
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    # Other methods and properties...
-
-    def __str__(self):
-        """ Method to override the default __str__ method """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height
-        )
-def display(self):
-    """ Method to display the rectangle with '#' characters, considering x and y """
-    for _ in range(self.__y):
-        print()
-    for _ in range(self.__height):
-        print(" " * self.__x + "#" * self.__width)#!/usr/bin/python3
-""" Module for Rectangle class """
-
-from models.base import Base
-
-class Rectangle(Base):
-    """ Class Rectangle that inherits from Base """
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ Constructor method """
-        super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-
-    @property
-    def width(self):
-        """ Getter method for width """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Setter method for width """
-        self.__width = value
-
-    @property
-    def height(self):
-        """ Getter method for height """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Setter method for height """
-        self.__height = value
-
-    @property#!/usr/bin/python3
-""" Module for Rectangle class """
-
-from models.base import Base
-
-class Rectangle(Base):
-    """ Class Rectangle that inherits from Base """
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ Constructor method """
-        super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
-
-    @property
-    def width(self):
-        """ Getter method for width """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Setter method for width """
-        self.__width = value
-
-    @property
-    def height(self):
-        """ Getter method for height """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Setter method for height """
-        self.__height = value
-
-    @property
-    def x(self):
-        """ Getter method for x """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """ Setter method for x """
-        self.__x = value
-
-    @property
-    def y(self):
-        """ Getter method for y """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """ Setter method for y """
-        self.__y = value
-
-if __name__ == "__main__":
-    r1 = Rectangle(10, 10, 10, 10)
-    print(r1)
-
-    r1.update(height=1)
-    print(r1)
-
-    r1.update(width=1, x=2)
-    print(r1)
-
-    r1.update(y=1, width=2, x=3, id=89)
-    print(r1)
-
-    r1.update(x=1, height=2, y=3, width=4)
-    print(r1)
-
-from models.rectangle import Rectangle
-
-class Square(Rectangle):
-    """ Class Square that inherits from Rectangle """
-
-    def __init__(self, size, x=0, y=0, id=None):
-        """ Constructor method """
-        super().__init__(size, size, x, y, id)
-
-    @property
-    def size(self):
-        """ Getter method for size """
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """ Setter method for size """
-        self.width = value
-        self.height = value
-
-    def __str__(self):
-        """ Method to override the default __str__ method """
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width
-        )
-@property
-def size(self):
-    """ Getter method for size """
-    return self.width
 
 @size.setter
 def size(self, value):
