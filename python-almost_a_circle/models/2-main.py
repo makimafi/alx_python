e62296c9a745a9259404cd09a668921fd7f2435b
maleda mesfin <markanmesfin555@gmail.com>
2023-12-20 20:43:23 +0300

    creat new file

--- python-almost_a_circle/models/2-main.py
+++ python-almost_a_circle/models/2-main.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-""" Module for Rectangle class """
-
-from models.base import Base
-
-class Rectangle(Base):
-    """ Class Rectangle that inherits from Base """
-
+""" 2-main """
+class Rectangle:
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Constructor method """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        self.id = id
 
     @property
     def width(self):
-        """ Getter method for width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Setter method for width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -30,12 +22,10 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """ Getter method for height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Setter method for height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -44,12 +34,10 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """ Getter method for x """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """ Setter method for x """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -58,14 +46,36 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """ Getter method for y """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """ Setter method for y """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+if __name__ == "__main__":
+
+    try:
+        Rectangle(10, "2")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
