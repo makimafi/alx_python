1b62eb66a1213c4ff915cd62d80e066a725cffc0
maleda mesfin <markanmesfin555@gmail.com>
2024-01-24 22:54:46 +0300

    requirement error fix

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -1,33 +1,37 @@
+#!/usr/bin/python3
+"""
+Start a Flask web application
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
-
+    """Display 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
-
+    """Display 'HBNB'"""
+    return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    return 'C ' + text.replace('_', ' ')
+def c_text(text):
+    """Display 'C' followed by the value of the text variable"""
+    return "C " + text.replace("_", " ")
 
-
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/', strict_slashes=False)
-def python_route(text='is cool'):
-    return 'Python ' + text.replace('_', ' ')
-
+def python_text(text):
+    """Display 'Python' followed by the value of the text variable"""
+    return "Python " + text.replace("_", " ")
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    return '{} is a number'.format(n)
-
+def number(n):
+    """Display 'n is a number' only if n is an integer"""
+    return "{} is a number".format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
