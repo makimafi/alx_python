02d33adc4c3d389a8c7c6e2ad350fd6357fe88f3
maleda mesfin <markanmesfin555@gmail.com>
2024-01-24 19:38:13 +0300

    creat new file

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route to display 'Hello HBNB!'
+
+    Returns:
+        str: Hello HBNB!
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
