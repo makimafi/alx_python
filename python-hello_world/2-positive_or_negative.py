29e3647df4b5a487f0fc3d2cdb801affdcd67443
maleda mesfin <markanmesfin555@gmail.com>
2024-01-25 16:44:59 +0300

    corection

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,5 +1,15 @@
-#!/usr/bin/python3
-for i in range(len(x)):
-    if i=o
-    print("{}, {}.format(len(x),  [-1])")
-     
+# 2-positive_or_negative.py
+
+# Importing required module
+import random
+
+# Assigning a random signed number to the variable number
+number = random.randint(-10, 10)
+
+# Checking if the number is positive, negative, or zero
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
