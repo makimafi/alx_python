d78352d8286573f524fa0ceaf5696a1358a3c516
maleda mesfin <markanmesfin555@gmail.com>
2024-01-26 22:19:48 +0300

    new file

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+class Square:
+    """
+    Square class represents a square with a size attribute.
 
+    Attributes:
+        __size (int): Size of the square.
 
-"class Square":
-    """Represents a square."""
-
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+    """
     def __init__(self, size=0):
-        """Initializes a new Square.
+        """
+        Initializes a new instance of the Square class.
 
         Args:
-            size (int): The size of the square.
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
-    def area(self):
-        """Computes the area of the square."""
-        return self.__size ** 2
+# Uncomment the following lines if you want to run the sample script
+# my_square_1 = Square(3)
+# print(type(my_square_1))
+# print(my_square_1.__dict__)
+
+# my_square_2 = Square()
+# print(type(my_square_2))
+# print(my_square
