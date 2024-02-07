8b89cefa8da0918442f7b75d92ec549767a380b5
maleda mesfin <markanmesfin555@gmail.com>
2024-02-07 19:39:13 +0300

    new file

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
 def no_c(my_string):
+    """Removes all occurrences of 'c' and 'C' from the given string."""
     new_string = ""
     for char in my_string:
         if char.lower() != 'c':
             new_string += char
     return new_string
 
-# Test cases
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+if __name__ == "__main__":
+    print(no_c("Holberton School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
