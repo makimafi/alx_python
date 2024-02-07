412487407f6d9f6a70ad041441e345e219938750
maleda mesfin <markanmesfin555@gmail.com>
2024-02-07 15:24:59 +0300

    new file

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -33,19 +33,3 @@ if __name__ == "__main__":
     print(pow(98, 0))
     print(pow(100, -2))
     print(pow(-4, 5))
-
-def pow(a, b):
-    if b == 0:
-        return 1
-    result = 1
-    if b > 0:
-        for _ in range(b):
-            result *= a
-    else:
-        for _ in range(-b):
-            result /= a
-    return round(result, 10)  # Round the result to 10 decimal places
-
-# Test cases
-print(pow(10, -2))
-print(pow(-98, -10))
