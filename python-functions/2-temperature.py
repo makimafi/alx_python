434b7c373189128db39278dde69f6521c9e0454b
maleda mesfin <markanmesfin555@gmail.com>
2024-02-07 15:08:28 +0300

    new file

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python3
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        a = 1 / a
+        b = -b
+    result = 1
+    while b > 0:
+        if b % 2 == 1:
+            result *= a
+        a *= a
+        b //= 2
+    return result
 
-def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
-    return celsius
+# Test cases
+print(pow(2, 2))     # Output: 4
+print(pow(98, 2))    # Output: 9604
+print(pow(98, 0))    # Output: 1
+print(pow(100, -2))  # Output: 0.0001
+print(pow(-4, 5))    # Output: -1024
+print(pow(-98, -10)) # Output: very small positive number close to zero
