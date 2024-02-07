3eb43610684dbff5e709669d9e9e70eb94d53fe5
maleda mesfin <markanmesfin555@gmail.com>
2024-02-07 15:13:03 +0300

    new file

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -10,12 +10,8 @@ def pow(a, b):
             result *= a
         a *= a
         b //= 2
-    return result
+    return round(result, 10)  # Round the result to 10 decimal places
 
 # Test cases
-print(pow(2, 2))     # Output: 4
-print(pow(98, 2))    # Output: 9604
-print(pow(98, 0))    # Output: 1
-print(pow(100, -2))  # Output: 0.0001
-print(pow(-4, 5))    # Output: -1024
-print(pow(-98, -10)) # Output: very small positive number close to zero
+print(pow(10, -2))   # Output: 0.01
+print(pow(-98, -10))  # Output: 1.223881142e-20
