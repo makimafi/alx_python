d830ce6cb5ebaa0aa90b47702fb17d581356d46c
maleda mesfin <markanmesfin555@gmail.com>
2024-02-24 23:27:05 +0300

    new file

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,33 +1,62 @@
-#!/usr/bin/python3
-"""Export data in the JSON format."""
 import json
 import requests
-from sys import argv
 
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/users'
-    response = requests.get(url)
-    users = response.json()
+def fetch_todo():
+    """
+    Fetches TODO data from a mock API.
+    """
+    response = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = response.json()
+    return todos
 
-    user_tasks = {}
-    for user in users:
-        user_id = user.get('id')
-        username = user.get('username')
 
-        url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
-        response = requests.get(url)
-        tasks = response.json()
+def group_tasks_by_user(tasks):
+    """
+    Groups tasks by user ID.
+    """
+    grouped_tasks = {}
+    for task in tasks:
+        user_id = task['userId']
+        if user_id not in grouped_tasks:
+            grouped_tasks[user_id] = []
+        grouped_tasks[user_id].append({
+            'username': task['username'],
+            'task': task['title'],
+            'completed': task['completed']
+        })
+    return grouped_tasks
 
-        user_tasks[user_id] = []
-        for task in tasks:
-            task_dict = {
-                "username": username,
-                "task": task.get('title'),
-                "completed": task.get('completed'),
-            }
-            user_tasks[user_id].append(task_dict)
 
-    # Write data to JSON file
+def export_to_json(grouped_tasks):
+    """
+    Exports grouped tasks to JSON file.
+    """
     with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(user_tasks, json_file)
+        json.dump(grouped_tasks, json_file, indent=4)
+
+
+def main():
+    # Fetch TODO data
+    todos = fetch_todo()
+
+    # Group tasks by user ID
+    grouped_tasks = group_tasks_by_user(todos)
+
+    # Export to JSON
+    export_to_json(grouped_tasks)
+
+    # Test if all users exist in the output
+    all_users_found = len(grouped_tasks) == 10
+
+    # Test if all tasks are assigned to the correct user IDs
+    user_ids = [task['userId'] for task in todos]
+    user_ids_in_output = list(grouped_tasks.keys())
+    user_tasks_output_ok = user_ids == user_ids_in_output
+
+    print("All users found:", "OK" if all_users_found else "Failed")
+    print("User ID and Tasks output:", "OK" if user_tasks_output_ok else "Failed")
+
+
+if __name__ == "__main__":
+    main()
