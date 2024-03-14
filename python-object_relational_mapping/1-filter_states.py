dd57427c6cad4e3fd4eafa66d4e56ff846a778f2
maleda mesfin <markanmesfin555@gmail.com>
2024-03-14 11:28:27 +0300

    new file

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,40 +2,53 @@
 import sys
 import MySQLdb
 
+def fetch_states(username, password, db_name):
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=db_name, charset="utf8")
+
+        # Create a cursor object using cursor() method
+        cursor = db.cursor()
+
+        # Prepare SQL query to retrieve states starting with 'N' (case-insensitive)
+        sql_query = "SELECT * FROM states WHERE name LIKE 'N%' COLLATE utf8_general_ci ORDER BY id ASC"
+
+        # Execute the SQL command
+        cursor.execute(sql_query)
+        
+        # Fetch all the rows in a list of tuples
+        results = cursor.fetchall()
+        
+        return results
+
+    except Exception as e:
+        # Print error message if any
+        print("Error:", e)
+
+    finally:
+        # Disconnect from server
+        db.close()
+
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=db_name, charset="utf8")
+    # Fetch states from the database
+    states = fetch_states(username, password, db_name)
 
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
+    # Print the fetched states
+    for state in states:
+        print(state)
 
-    # Prepare SQL query to retrieve states starting with 'N' (case-insensitive)
-    sql_query = "SELECT * FROM states WHERE name LIKE 'N%' COLLATE utf8_general_ci ORDER BY id ASC"
-
-    try:
-        # Execute the SQL command
-        cursor.execute(sql_query)
-        # Fetch all the rows in a list of tuples
-        results = cursor.fetchall()
-
-        # Print the results
-        print("Correct output - case: 2 records\n")
-        for row in results:
-            print(row)
-    except Exception as e:
-        # Rollback in case there is any error
-        print("Error:", e)
-        db.rollback()
-
-    # Disconnect from server
-    db.close()
+    # Expected output for lowercase 'n'
+    expected_output = [
+        (2, 'nevada'),
+        (3, 'New York')
+    ]
 
     # Print the expected output
     print("\nExpected output - case: Lowercase n\n")
-    print("(2, 'nevada')")
-    print("(3, 'New York')")
+    for state in expected_output:
+        print(state)
