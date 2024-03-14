95904b3208a31eee8238e8131c027b9a1a82a746
maleda mesfin <markanmesfin555@gmail.com>
2024-03-14 10:41:54 +0300

    new file

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Take MySQL username, password, and database name from command line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
 
-    # Create a cursor object to interact with the database
+    # Create a cursor object using cursor() method
     cursor = db.cursor()
 
-    # Execute the query to select states starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    # Prepare SQL query to retrieve states starting with 'N'
+    sql_query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
 
-    # Fetch all the rows and print the results
-    for row in cursor.fetchall():
-        print(row)
+    try:
+        # Execute the SQL command
+        cursor.execute(sql_query)
+        # Fetch all the rows in a list of tuples
+        results = cursor.fetchall()
+        # Display the results
+        for row in results:
+            print(row)
+    except Exception as e:
+        # Rollback in case there is any error
+        print("Error:", e)
+        db.rollback()
 
-    # Close the cursor and database connection
-    cursor.close()
+    # Disconnect from server
     db.close()
